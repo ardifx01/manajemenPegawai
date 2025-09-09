6cde51fceaff3a09fdb249b7a14a733f696ca3a4
NBFiidly <113288089+NBFiidly@users.noreply.github.com>
2025-09-09 16:42:28 +0700

    Part5_Query,_Seeder_dan_Factory

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Pegawai;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,28 @@ Route::get('/pegawai', function() {
 
 Route::get('/test', function (){
     return redirect()->to('/');
+});
+
+Route::get('/coba_query', function() {
+
+    // $pegawai = Pegawai::all(); // menampilkan semua data
+    // dd($pegawai->toArray());
+
+    // $pegawai = Pegawai::find(75); //mencari data berdasarkan ID
+    // dd($pegawai->toArray());
+
+    // $pegawai = Pegawai::where('nama_pegawai', 'Azalea Nasyidah')->first(); // menampilkan data berdasarkan nama
+
+    // $pegawai = Pegawai::where('usia', '>', 20)->get(); // menampilkan data berdasarkan usia
+    // dd($pegawai->toArray());
+
+    // Pegawai::where('nama_pegawai', 'Azalea Nasyidah')->delete(); // Menghapus data berdasarkan nama
+
+    // Pegawai::destroy(1); // Menghapus data berdsarkan ID
+
+    // Pegawai::where('id', 50)->update([
+    //     'nama_pegawai' => 'Benjamin amin' // -> Update data berdasarkan ID
+    // ]);
 });
 
 Route::fallback(function () {
